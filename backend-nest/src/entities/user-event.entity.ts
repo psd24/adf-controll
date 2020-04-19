54b2c670923ca0c9e9bfaa6240296c7f57888b7f
davidsancho4 <davidsancho4@gmail.com>
2020-04-19 22:27:37 +0200

    Added reletion event vehicle

--- backend-nest/src/entities/user-event.entity.ts
+++ backend-nest/src/entities/user-event.entity.ts
@@ -1,5 +1,6 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToMany, JoinTable } from "typeorm";
 import { User } from "./user.entity";
+import { Vehicle } from './vehicle.entity';
 
 @Entity('event')
 export class UserEvent {
@@ -25,4 +26,8 @@ export class UserEvent {
     @ManyToMany(() => User, user => user.events)
     @JoinTable()
     users: User[];
+
+    @ManyToMany(() => Vehicle, vehicle => vehicle.events)
+    @JoinTable()
+    vehicle: Vehicle[];
 }
--- backend-nest/src/entities/vehicle.entity.ts
+++ backend-nest/src/entities/vehicle.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne, ManyToMany } from "typeorm";
 import { Organization } from './organization.entity';
+import { UserEvent } from "./user-event.entity";
 
 @Entity('vehicle')
 export class Vehicle {
@@ -17,6 +18,9 @@ export class Vehicle {
 
     @ManyToOne(type => Organization, organization => organization.vehicle)
     organization: Organization;
+
+    @ManyToMany(() => UserEvent, event => event.vehicle)
+    events: UserEvent[];
 
     constructor(name: string) {
         this.name = name;
