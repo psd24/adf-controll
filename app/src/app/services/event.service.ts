7bdabad04e1152a24e133256e8db0daa1b5ccbd6
davidsancho4 <davidsancho4@gmail.com>
2020-04-24 00:12:55 +0200

    Added service roles.

--- app/src/app/services/event.service.ts
+++ app/src/app/services/event.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
 import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,12 +10,24 @@ import { HttpClient } from '@angular/common/http';
 export class EventService {
 
   userToken: string;
+  httpClient: any;
 
   constructor(private auth: AuthService, private http: HttpClient ) { }
 
   getEvents(){
     this.leerToken();
     return this.http.get('http://localhost:3000/events');
+  }
+
+  readFrame(): Observable<any> {
+    return this.http.get('http://localhost:3000/roles').pipe(
+      map(res => {
+        return res;
+      }),
+      catchError(error => {
+        return throwError(error.error)
+      })
+    );
   }
 
   private leerToken() {
