b9796dc311bd0a654b64753ab3cde2b4dcaada33
davidsancho4 <davidsancho4@gmail.com>
2020-05-13 19:57:39 +0200

    Delete app config

--- backend-nest/src/app.config.ts
+++
@@ -1,7 +0,0 @@
-export class AppConfig {
-
-    public static telegramToken     = '1256671122:AAGsnSGwWRFz3lIA1gh9totgSwHuLgpnmkE';
-    public static dbUser            = 'root';
-    public static dbPassword        = '';
-    public static dbName            = 'adfcontroll';
-}
