85a3dc07ea0080b19de8e8853ae1ba594e9d17ee
davidsancho4 <davidsancho4@gmail.com>
2020-05-16 20:40:17 +0200

    Added logout

--- frontend/src/app/layout/header/header.component.ts
+++ frontend/src/app/layout/header/header.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { first } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  user;
+
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) { }
 
   ngOnInit(): void {
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.user = users;
+    });
+  }
+
+  logout() {
+    this.authenticationService.logout();
   }
 
 }
