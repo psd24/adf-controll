8dcf59f188592db822cdb52147c004d079109da2
David <you@example.com>
2020-05-26 09:29:45 +0200

    ci.yml added for automatic build

--- backend-nest/dist/app.config.js
+++ backend-nest/dist/app.config.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class AppConfig {
 }
 exports.AppConfig = AppConfig;
-AppConfig.telegramToken = '1268044143:AAFN8AAV9Fjyp7TJOGMUSGwGCzO4Qhrtzwk';
-AppConfig.dbUser = 'root';
-AppConfig.dbPassword = '';
+AppConfig.telegramToken = '1256671122:AAGsnSGwWRFz3lIA1gh9totgSwHuLgpnmkE';
+AppConfig.dbUser = 'david';
+AppConfig.dbPassword = 'nopass123';
 AppConfig.dbName = 'adfcontroll';
 //# sourceMappingURL=app.config.js.map
