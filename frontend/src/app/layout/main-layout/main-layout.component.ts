82271cc63418c0336c360d9191a3ed18f85f73e8
davidsancho4 <davidsancho4@gmail.com>
2020-06-09 21:46:56 +0200

    Added new style.

--- frontend/src/app/layout/main-layout/main-layout.component.ts
+++ frontend/src/app/layout/main-layout/main-layout.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-main-layout',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLayoutComponent implements OnInit {
 
-  constructor() { }
+  user;
+
+  constructor(
+    private userService: UserService,
+    private authenticationService: AuthenticationService
+  ) { }
+  
 
   ngOnInit(): void {
+    this.userService.getAll().pipe(first()).subscribe(users => {
+      this.user = users;
+    });
   }
+
+  logout() {
+    this.authenticationService.logout();
+  }
+
 
 }
